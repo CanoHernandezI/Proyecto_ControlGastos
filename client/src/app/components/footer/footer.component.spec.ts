eff411eba1e48e9c93351f90ee0b82b511995110
“RamirezLuisManuel” <luismanuelr245@gmail.com>
2025-02-07 16:19:40 -0600

    Footer con atajos

--- client/src/app/components/footer/footer.component.spec.ts
+++ client/src/app/components/footer/footer.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FinanzasComponent } from './finanzas.component';
+import { FooterComponent } from './footer.component';
 
-describe('FinanzasComponent', () => {
-  let component: FinanzasComponent;
-  let fixture: ComponentFixture<FinanzasComponent>;
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [FinanzasComponent]
+      declarations: [FooterComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(FinanzasComponent);
+    fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
